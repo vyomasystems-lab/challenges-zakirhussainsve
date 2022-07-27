f4f475524f4cb3d955c53088f5807b70e13366dd
zakirhussainsve <zakirhussainsve@gmail.com>
2022-07-27 07:46:37 +0000

    v5

--- level1_design1/mux_model/mux_model.py
+++ level1_design1/mux_model/mux_model.py
@@ -1,6 +1,7 @@
 # python program to implement multiplexer
 # Function to print output
-def mux_model(I,s) -> int:
+def mux_model(s,inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8, inp9, inp10, inp11, inp12, inp13, inp14, inp15, inp16, inp17,
+           inp18, inp19, inp20, inp21, inp22, inp23, inp24, inp25, inp26,inp27, inp28, inp29, inp30):
 	#decimal value of s
 	d= (s[0] * 16) + (s[1] * 8) + (s[2] * 4) + (s[3] * 2) + (s[4] * 1)
 	# getting the output of decimal value from inputs
