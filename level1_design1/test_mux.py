da7c0eb2d39964a2171b1a3e8b1246eab2de6b17
zakirhussainsve <zakirhussainsve@gmail.com>
2022-07-27 09:23:27 +0000

    v33

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -13,39 +13,75 @@ async def test_mux(dut):
 
     for i in range(5):
 
-        dut.inp0.value=random.randint(0, 3)
-        dut.inp1.value=random.randint(0, 3)
-        dut.inp2.value=random.randint(0, 3)
-        dut.inp3.value=random.randint(0, 3)
-        dut.inp3.value=random.randint(0, 3)
-        dut.inp5.value=random.randint(0, 3)
-        dut.inp6.value=random.randint(0, 3)
-        dut.inp7.value=random.randint(0, 3)
-        dut.inp8.value=random.randint(0, 3)
-        dut.inp9.value=random.randint(0, 3)
-        dut.inp10.value=random.randint(0, 3)
-        dut.inp11.value=random.randint(0, 3)
-        dut.inp12.value=random.randint(0, 3)
-        dut.inp13.value=random.randint(0, 3)
-        dut.inp13.value=random.randint(0, 3)
-        dut.inp15.value=random.randint(0, 3)
-        dut.inp16.value=random.randint(0, 3)
-        dut.inp17.value=random.randint(0, 3)
-        dut.inp18.value=random.randint(0, 3)
-        dut.inp19.value=random.randint(0, 3)
-        dut.inp20.value=random.randint(0, 3)
-        dut.inp21.value=random.randint(0, 3)
-        dut.inp22.value=random.randint(0, 3)
-        dut.inp23.value=random.randint(0, 3)
-        dut.inp23.value=random.randint(0, 3)
-        dut.inp25.value=random.randint(0, 3)
-        dut.inp26.value=random.randint(0, 3)
-        dut.inp27.value=random.randint(0, 3)
-        dut.inp28.value=random.randint(0, 3)
-        dut.inp29.value=random.randint(0, 3)
-        dut.inp30.value=random.randint(0, 3)
+        
+        inp0=random.randint(0, 3)
+        inp1=random.randint(0, 3)
+        inp2=random.randint(0, 3)
+        inp3=random.randint(0, 3)
+        inp4=random.randint(0, 3)
+        inp5=random.randint(0, 3)
+        inp6=random.randint(0, 3)
+        inp7=random.randint(0, 3)
+        inp8=random.randint(0, 3)
+        inp9=random.randint(0, 3)
+        inp10=random.randint(0, 3)
+        inp11=random.randint(0, 3)
+        inp12=random.randint(0, 3)
+        inp13=random.randint(0, 3)
+        inp14=random.randint(0, 3)
+        inp15=random.randint(0, 3)
+        inp16=random.randint(0, 3)
+        inp17=random.randint(0, 3)
+        inp18=random.randint(0, 3)
+        inp19=random.randint(0, 3)
+        inp20=random.randint(0, 3)
+        inp21=random.randint(0, 3)
+        inp22=random.randint(0, 3)
+        inp23=random.randint(0, 3)
+        inp24=random.randint(0, 3)
+        inp25=random.randint(0, 3)
+        inp26=random.randint(0, 3)
+        inp27=random.randint(0, 3)
+        inp28=random.randint(0, 3)
+        inp29=random.randint(0, 3)
+        inp30=random.randint(0, 3)
+        sel=random.randint(0, 31)
+        
+        
+        
+        dut.inp0.value=inp0
+        dut.inp1.value=inp1
+        dut.inp2.value=inp2
+        dut.inp3.value=inp3
+        dut.inp4.value=inp4
+        dut.inp5.value=inp5
+        dut.inp6.value=inp6
+        dut.inp7.value=inp7
+        dut.inp8.value=inp8
+        dut.inp9.value=inp9
+        dut.inp10.value=inp10
+        dut.inp11.value=inp11
+        dut.inp12.value=inp12
+        dut.inp13.value=inp13
+        dut.inp14.value=inp14
+        dut.inp15.value=inp15
+        dut.inp16.value=inp16
+        dut.inp17.value=inp17
+        dut.inp18.value=inp18
+        dut.inp19.value=inp19
+        dut.inp20.value=inp20
+        dut.inp21.value=inp21
+        dut.inp22.value=inp22
+        dut.inp23.value=inp23
+        dut.inp24.value=inp24
+        dut.inp25.value=inp25
+        dut.inp26.value=inp26
+        dut.inp27.value=inp27
+        dut.inp28.value=inp28
+        dut.inp29.value=inp29
+        dut.inp30.value=inp30
 
-        dut.sel.value=random.randint(0, 31)
+        dut.sel.value=sel
        
 
         await Timer(2, units="ns")
