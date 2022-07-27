6de72023af6a96155151193fb6746d05472857fd
zakirhussainsve <zakirhussainsve@gmail.com>
2022-07-27 08:02:33 +0000

    v7

--- level1_design1/mux_model/mux_model.py
+++ level1_design1/mux_model/mux_model.py
@@ -1,10 +1,73 @@
 # python program to implement multiplexer
 # Function to print output
-def mux_model(s,inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8, inp9, inp10, inp11, inp12, inp13, inp14, inp15, inp16, inp17,
+def mux_model(sel,inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8, inp9, inp10, inp11, inp12, inp13, inp14, inp15, inp16, inp17,
            inp18, inp19, inp20, inp21, inp22, inp23, inp24, inp25, inp26,inp27, inp28, inp29, inp30):
 	#decimal value of s
-	d= (s[0] * 16) + (s[1] * 8) + (s[2] * 4) + (s[3] * 2) + (s[4] * 1)
-	# getting the output of decimal value from inputs
-	b = I[d];
-    return b
+	if sel == 0:
+        y = inp0
+	elif sel == 1:
+        y = inp1
+    elif sel == 2:
+        y = inp2
+    elif sel == 3:
+        y = inp3
+    elif sel == 4:
+        y= inp4
+    elif sel == 5:
+        y= inp5
+    elif sel == 6:
+        y = inp6
+    elif sel == 7:
+        y = inp7
+    elif sel == 8:
+        y = inp8
+    elif sel == 9:
+        y= inp9
+    elif sel == 10:
+        y= inp10
+    elif sel == 11:
+        y= inp11
+    elif sel == 12:
+        y= inp12
+	elif sel == 13:
+        y = inp13
+    elif sel == 14:
+        y = inp14
+    elif sel == 15:
+        y = inp15
+    elif sel == 16:
+        y= inp16
+    elif sel == 17:
+        y= inp17
+    elif sel == 18:
+        y = inp18
+    elif sel == 19:
+        y = inp19
+    elif sel == 20:
+        y = inp20
+    elif sel == 21:
+        y= inp21
+    elif sel == 22:
+        y= inp22
+    elif sel == 23:
+        y= inp23
+    elif sel == 24:
+        y= inp24
+	elif sel == 25:
+        y = inp25
+    elif sel == 26:
+        y = inp26
+    elif sel == 27:
+        y = inp27
+    elif sel == 28:
+        y= inp28
+    elif sel == 29:
+        y= inp29
+    elif sel == 30:
+        y = inp30
+    elif sel == 31:
+        y = inp31
+    else:
+        y= 0
+    return y
 	
