2d9330f9a8cd5e72705f212c068f5e60a91ac8aa
zakirhussainsve <zakirhussainsve@gmail.com>
2022-07-27 13:37:01 +0000

    success v1

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -1,9 +1,77 @@
 # See LICENSE.vyoma for details
 import random
 
-from mux_model import mux_model
+
+#from mux_model import mux_model
 import cocotb
 from cocotb.triggers import Timer
+
+def mux_model(sel,inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8, inp9, inp10, inp11, inp12, inp13, inp14, inp15, inp16, inp17, inp18, inp19, inp20, inp21, inp22, inp23, inp24, inp25, inp26,inp27, inp28, inp29, inp30):
+  if sel == 0:
+    y = inp0
+  elif sel == 1:
+      y = inp1
+  elif sel == 2:
+    y = inp2
+  elif sel == 3:
+    y = inp3
+  elif sel == 4:
+    y = inp4
+  elif sel == 5:
+    y = inp5
+  elif sel == 6:
+    y = inp6
+  elif sel == 7:
+    y = inp7
+  elif sel == 8:
+    y = inp8
+  elif sel == 9:
+    y= inp9
+  elif sel == 10:
+    y = inp10
+  elif sel == 11:
+    y = inp11
+  elif sel == 12:
+    y = inp12
+  elif sel == 13:
+    y = inp13
+  elif sel == 14:
+    y = inp14
+  elif sel == 15:
+    y = inp15
+  elif sel == 16:
+    y = inp16
+  elif sel == 17:
+    y = inp17
+  elif sel == 18:
+    y = inp18
+  elif sel == 19:
+    y = inp19
+  elif sel == 20:
+    y = inp20
+  elif sel == 21:
+    y = inp21
+  elif sel == 22:
+    y = inp22
+  elif sel == 23:
+    y = inp23
+  elif sel == 24:
+    y = inp24
+  elif sel == 25:
+    y = inp25
+  elif sel == 26:
+    y = inp26
+  elif sel == 27:
+    y = inp27
+  elif sel == 28:
+    y = inp28
+  elif sel == 29:
+    y = inp29
+  elif sel == 30:
+    y = inp30
+  else:
+    y = 0
+  return y
 
 @cocotb.test()
 async def test_mux(dut):
@@ -81,7 +149,6 @@ async def test_mux(dut):
 
         dut.sel.value=sel
        
-
         await Timer(2, units="ns")
 
         if(dut.out.value == mux_model(sel,inp0, inp1, inp2, inp3, inp4, inp5, inp6, inp7, inp8, inp9, inp10, inp11, inp12, inp13, inp14, inp15, inp16, inp17,inp18, inp19, inp20, inp21, inp22, inp23, inp24, inp25, inp26,inp27, inp28, inp29, inp30)):
