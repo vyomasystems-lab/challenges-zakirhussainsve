4d879698549cda29a5bd2d3a5d94dcd0669071ed
zakirhussainsve <zakirhussainsve@gmail.com>
2022-07-27 07:29:07 +0000

    v3

--- level1_design1/mux_model.py
+++ level1_design1/mux_model.py
@@ -1,6 +1,6 @@
 # python program to implement multiplexer
 # Function to print output
-def Multiplexer(I,s):
+def Multiplexer(I,s) -> int:
 	#decimal value of s
 	d= (s[0] * 16) + (s[1] * 8) + (s[2] * 4) + (s[3] * 2) + (s[4] * 1)
 	# getting the output of decimal value from inputs
